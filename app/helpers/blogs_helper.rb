433fb819b7396ff8bc70bb5d41673a63aab96f37
Koby Willis <koby_willis@hotmail.com>
2019-03-12 14:56:13 -0600

    Added new navbar styles for blog

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,6 +1,6 @@
 module BlogsHelper
   def gravatar_helper user
-    image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 40
+    image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
   end
 
   class CodeRayify < Redcarpet::Render::HTML
@@ -10,21 +10,20 @@ module BlogsHelper
   end
 
   def markdown(text)
-    coderayified = CodeRayify.new(filer_html: true, hard_wrap: true)
+    coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
 
     options = {
       fenced_code_blocks: true,
       no_intra_emphasis: true,
-      autolinks: true,
+      autolink: true,
       lax_html_blocks: true,
-      }
+    }
 
     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
     markdown_to_html.render(text).html_safe
   end
 
-  def blog_status_color blog 
-    if blog.draft?
-      "color: red;"
+  def blog_status_color blog
+    'color: red;' if blog.draft?
   end
 end
